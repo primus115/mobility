4676aa1326f88e28d9a7eb81f052ded2a3f6f925
primus115 <za.primoz@gmail.com>
2019-01-03 01:07:46 +0100

    first interaction with ethereum from runner.py

--- runner.py
+++ runner.py
@@ -9,7 +9,7 @@
 
 # @file    runner.py
 # @author  Primoz Zajec
-# @date    2018-11-
+# @date    2018-11-17
 # @version $Id$
 
 from __future__ import absolute_import
@@ -23,6 +23,12 @@ import random
 import paho.mqtt.client as mqtt
 import time
 import json
+from threading import Thread
+
+from web3 import Web3, HTTPProvider
+from getpass import getpass  
+
+RINKEBY = "https://rinkeby.infura.io/v3/23d1d5956dc54a068ab6887ef8e711b3"
 
 # we need to import python modules from the $SUMO_HOME/tools directory
 if 'SUMO_HOME' in os.environ:
@@ -95,38 +101,6 @@ def run():
             topic = "pos/slovenia/ljubljana"
             payload = json.dumps({"id":carName, "lon":pos[carName][0], "lat":pos[carName][1]})
             
-
-#            edgeIDt1 = traci.simulation.convertRoad(14.48367, 46.04032, True)
-#            print("::::::: ", edgeIDt1)
-#            edgeIDt2 = traci.simulation.convertRoad(14.48507, 46.04064, True)
-#            print("::::::: ", edgeIDt2)
-#            edgeIDt3 = traci.simulation.convertRoad(14.49044, 46.04335, True)
-#            print("::::::: ", edgeIDt3)
-##            traci.vehicle.getAdaptedTraveltime("taxi1", time, edgeID)
-#            print("traveltime", traci.vehicle.getAdaptedTraveltime("taxi1", 0, edgeIDt1[0]))
-#            print("traveltime", traci.vehicle.getAdaptedTraveltime("taxi1", 0, edgeIDt2[0]))
-#            print("adaptedTraveltime after adaption in interval (check time 0)", traci.edge.getTraveltime(edgeIDt1[0]))
-#            print("adaptedTraveltime after adaption in interval (check time 0)", traci.edge.getTraveltime(edgeIDt2[0]))
-#            print("adaptedTraveltime after adaption in interval (check time 0)", traci.edge.getTraveltime(edgeIDt3[0]))
-#
-#            route1 = traci.simulation.findRoute("279297476",edgeIDt1[0]).edges
-#            route2 = traci.simulation.findRoute("279297476",edgeIDt2[0]).edges
-#            print("kkkkk: ",route)
-#            time = 0
-#            for edge in route1 :
-#               time += traci.edge.getTraveltime(edge)
-#            print("time111: ",time)
-#            time = 0
-#            for edge in route2 :
-#               time += traci.edge.getTraveltime(edge)
-#            print("time2222: ",time)
-
-
-
-
-#           print("convertGeo2D", traci.simulation.convertGeo(pos["taxi1"][0], pos["taxi1"][1], True))
-#            print("------------")
-#            print(payload)
             if(step%1 == 0):
                 mqttClient.publish(topic, payload)
 #            print(pos[v])
@@ -141,34 +115,6 @@ def run():
             state = [""]
             stateAction(state_)
 
-##            for veh in ["taxi1"]:#, "taxi2", "taxi3"]:
-# 
-#            print("----------------------------------")
-#            print("We are in 40s")
-#            print("----------------------------------")
-##            if traci.vehicle.getSpeed("right_0") == 0:
-##                traci.vehicle.resume("right_0")
-#            # changeTarget(vehicleID, edgeID) - can make turns
-##            traci.vehicle.changeTarget("right_0", "04")
-#            # setRoute(self, vehID, edgeList) ex:setRoute('1', ['1', '2', '4'])
-##            traci.vehicle.setRoute("right_0", ["10", "03", "30"])
-#            # setRouteID(self, vehID, routeID)
-##            traci.vehicle.setRouteID("right_0", "down")
-##            newRoute = traci.simulation.findRoute("279297476", "39726119#0").edges
-##            edgeID = traci.simulation.convertRoad(14.49634, 46.04794, True)
-##            edgeID = traci.simulation.convertRoad(14.49278, 46.04561, True)
-#            edgeID = traci.simulation.convertRoad(14.47917, 46.04494, True)
-##            newRoute = traci.simulation.findRoute("279297476", edgeID[0]).edges
-#            currentEdge = traci.vehicle.getRoadID("taxi1")
-#            print("ROAD:::", currentEdge)
-###            newRoute = traci.simulation.findRoute(currentEdge, "66478404#2").edges
-#            newRoute = traci.simulation.findRoute(currentEdge, edgeID[0]).edges
-#            print("nova rutaaaaa: ", newRoute)
-#            traci.vehicle.setRoute("taxi1", newRoute)
-#            traci.vehicle.setStop("taxi1", edgeID[0], flags=traci.constants.STOP_PARKING)
-####            traci.vehicle.setStop("taxi1", "66478404#2", flags=traci.constants.STOP_PARKING)
-#            if traci.vehicle.getSpeed("taxi1") == 0:
-#                traci.vehicle.resume("taxi1")
         step += 1
     mqttClient.disconnect()
     mqttClient.loop_stop()
@@ -198,6 +144,7 @@ def stateAction(state):
                 mtime = traci.edge.getTraveltime(edge)
                 if (mtime > 40.0):
                     print("STRANGE EST. TIME FOR NODE: {}!!!!!!!!!!!!!!!!!!!!!!!!!!".format(edge))
+                    print(mtime)
                     mtime = 0.0
                 route1time += mtime
             duration = route1time + route2time
@@ -220,8 +167,8 @@ def stateAction(state):
         print("destEdgeID :", destEdgeID)
         route2 = traci.simulation.findRoute(appEdgeID[0],destEdgeID[0]).edges
 
-        print("For the vehicle :{}".format(state[1]))
-        vehEdgeID = traci.vehicle.getRoadID(state[1])
+        print("For the vehicle :{}".format(carName))
+        vehEdgeID = traci.vehicle.getRoadID(carName)
         print("vehEdgeID :", vehEdgeID)
         route1 = traci.simulation.findRoute(vehEdgeID,appEdgeID[0]).edges
         print("route1 :", route1)
@@ -229,14 +176,57 @@ def stateAction(state):
         #getDistanceRoad(self, edgeID1, pos1, edgeID2, pos2, isDriving=False)
         distance1 = traci.simulation.getDistanceRoad(vehEdgeID, 0, appEdgeID[0], 0, isDriving=True)
         distance2 = traci.simulation.getDistanceRoad(appEdgeID[0], 0, destEdgeID[0], 0, isDriving=True)
-        print("-AAA--a-a-a--a-a-a--a")
         print(distance1)
         print(distance2)
+        distance = int(distance1 + distance2)
 
-        if traci.vehicle.getSpeed(state[1]) == 0:
-            traci.vehicle.setRoute(state[1], route1)
-            traci.vehicle.setStop(state[1], appEdgeID[0], flags=traci.constants.STOP_PARKING)
-            traci.vehicle.resume(state[1])
+        ###############   ETHEREUM CONTRACT - SET DISTANCE   ###########
+        with open("./ethereum/build/MobilityAccountABI.json") as f:
+            info_json = json.load(f)
+        abi = info_json
+
+        w3 = Web3(HTTPProvider(RINKEBY))
+        profile = w3.eth.contract(address=getProfileADDR(carName), abi=abi)
+        pw = getpass(prompt='Enter the password for decryption: ')
+
+        with open('./ethereum/{}.json'.format(carName)) as keyfile:
+            encrypted_key = keyfile.read()
+        pk = w3.eth.account.decrypt(encrypted_key, pw)
+
+        transaction = {
+#            'value': Web3.toWei(0.001, "ether"),
+            'gas': 500000,
+            'gasPrice': w3.eth.gasPrice,
+            'nonce': w3.eth.getTransactionCount(getVehicleADDR(carName))
+        }
+        try:
+            txn = profile.functions.setDistance(distance).buildTransaction(transaction)
+            signed_txn = w3.eth.account.signTransaction(txn, private_key=pk)
+            w3.eth.sendRawTransaction(signed_txn.rawTransaction)
+        except Exception as e:
+            print(e)
+        ################################################################
+        print("nakazi:")
+        print(distance * 0.00001)
+
+
+        if traci.vehicle.getSpeed(carName) == 0:
+            traci.vehicle.setRoute(carName, route1)
+            traci.vehicle.setStop(carName, appEdgeID[0], flags=traci.constants.STOP_PARKING)
+            traci.vehicle.resume(carName)
+
+def getVehicleADDR(_carName):
+    if _carName == "taxi1":
+        return '0xB76AC1f39edCa9087dBcaa832B3169cCE0E8Dc65'
+    elif _carName == "taxi2":
+        return '0xEFB3164881f0B9E3b8b498951A9047fD24dACE4c'
+
+def getProfileADDR(_carName):
+    if _carName == "taxi1":
+        return '0xf9a4eb92B62eDd28ccB9165eC5271029422E5D4C'
+    elif _carName == "taxi2":
+        return '0x6811fd581988561A2b2292f401400e3e05614987'
+
 
 
 def getOptions():
